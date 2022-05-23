b61a52523dfff33b4ca8685b44e41f5c93d61e78
ngonhan71 <ngohuunhan10@gmail.com>
2022-05-23 21:30:54 +0700

    fix .env

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:5000/api/v1/',
+  baseURL: process.env.REACT_APP_SERVER_URL,
   headers: {
     "Content-Type": "application/json",
   },
